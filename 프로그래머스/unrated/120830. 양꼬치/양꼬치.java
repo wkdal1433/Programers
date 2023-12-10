a43e7d5fef761c78553ee2d88063e29e20f35899
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2023-12-10 23:00:37 +0900

    [level unrated] Title: 양꼬치, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int count = n/10;
+        int answer = n*12000 +2000*k-2000*count;
+        
+        
+        return answer;
+    }
+}
