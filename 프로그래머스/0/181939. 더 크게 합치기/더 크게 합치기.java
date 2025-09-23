0f7d4e71bd8e525eacf0cafc0f7dbdbc36156620
장준수 <126257712+wkdal1433@users.noreply.github.com>
2025-09-23 13:08:01 +0900

    [level 0] Title: 더 크게 합치기, Time: 13.69 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,17 +1,8 @@
 class Solution {
     public int solution(int a, int b) {
+        int c = Integer.parseInt(a+""+b);
+        int d = Integer.parseInt(b+""+a);
         int answer = 0;
-        String c = "";
-        c += a;
-        c += b;
-        String d = "";
-        d +=b;
-        d += a;
-        a = Integer.parseInt(c);
-        b = Integer.parseInt(d);
-        if(a>b) answer = a;
-        else answer = b;
-        
-        return answer;
+        return answer = (c>d)?c:d; 
     }
 }
