d27cf1a896b4e87075303221ad175545582d6da3
장준수 <126257712+wkdal1433@users.noreply.github.com>
2025-09-24 12:35:32 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int number, int n, int m) {
         int answer = 0;
-        if(number%n==0){
-            answer = (number%m==0)? 1:0;
-        }
-        return answer;
+        
+        return answer = (number%n==0&&number%m==0)? 1:0;
+        
     }
 }
